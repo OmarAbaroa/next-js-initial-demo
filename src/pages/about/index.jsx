9bec5e19315ca49680556a093d7ae201bb810a5e
OmarAbaroa <alvaro1295@hotmail.com>
2023-05-17 22:15:28 -0600

    Adding layout

---
+++ src/pages/about/index.jsx
@@ -0,0 +1,30 @@
+import Link from 'next/link'
+import { MainLayout } from '@/components/layouts/MainLayout'
+import { DarkLayout } from '@/components/layouts/DarkLayout'
+
+export default function About(){
+  return (
+    <>
+      <h1>About Page</h1>
+
+      <h1 className={'title'}>
+        Ir a <Link href="contact">Contact</Link>
+      </h1>
+
+      <p className={'description'}>
+        Get started by editing{' '}
+        <code className={'code'}>pages/about/index.jsx</code>
+      </p>
+    </>
+  )
+}
+
+About.getLayout = function getLayout ( page ) {
+  return (
+    <MainLayout>
+      <DarkLayout>
+        { page }
+      </DarkLayout>
+    </MainLayout>
+  )
+}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,42 +1,21 @@
-import styles from '../styles/about.module.css'
-import Image from 'next/image'
-import Head from 'next/head'
+import Link from 'next/link'
+import { MainLayout } from '../components/layouts/MainLayout'
 
 export default function Home(){
   return (
-    <div>
-      <Head>
-        <title>
-          Home Omar
-        </title>
-      </Head>
-      
-      <main className={styles.main}>
-        <div className={styles.center}>
-          <p>
-            <Image
-              className={styles.logo}
-              src="/next.svg"
-              alt="Next.js Logo"
-              width={180}
-              height={37}
-              priority
-            />
-          </p>
-        </div>
+    <MainLayout>
 
-        <div style={{textAlign: "center" }}>
-          <a href='about'>
-            <h2>
-              Ir a {' '}
-              <text style={{color: "blue"}}>
-                About
-              </text>
-            </h2>
-          </a>
-        </div>
-      </main>
-    </div>
-    
+      <h1>Home Page</h1>
+
+      <h1 className={'title'}>
+        Ir a <Link href="about">About</Link>
+      </h1>
+
+      <p className={'description'}>
+        Get started by editing{' '}
+        <code className={'code'}>pages/index.jsx</code>
+      </p>
+
+    </MainLayout>
   )
 }
